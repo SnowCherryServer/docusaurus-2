602d535e5a9f79c8aa43345f6e1fb6722d336d60
SnowyMC <Snow@mc233.fun>
2025-02-03 09:13:33 +0800

    修改导航栏标题为“StarLand Wiki”

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '首页',
+        title: 'StarLand Wiki',
         logo: {
           alt: 'StarLand Logo',
           src: 'img/logo.svg',
